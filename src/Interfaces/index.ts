a2eab70d2c8c290f01ee4aaead3c217144af4f42
Nataly Dugasova <dugasova@gmail.com>
2024-10-03 17:12:30 +0300

    Fixed typos

--- src/Interfaces/index.ts
+++ src/Interfaces/index.ts
@@ -54,24 +54,24 @@ interface IAutor {
 }
 interface IBookService {
   getBookById(id: number): IBook | undefined;
-  getAutorById(id: number): IAutor | undefined;
+  getAuthorById(id: number): IAutor | undefined;
   getAllBooks(): IBook[];
   getAllAuthors(): IAutor[];
 }
-const books = [
+const books: IBook[] = [
   { id: 1, title: 'My Brilliant Friend', authorId: 1, publishedYear: 2018 },
   { id: 2, title: 'Madly, Deeply', authorId: 2, publishedYear: 2015 },
   { id: 3, title: 'The Gates of Europe', authorId: 3, publishedYear: 2021 },
 ];
 
-const authors = [
+const authors: IAutor[] = [
   { id: 1, name: 'Elena Ferante', birthYear: 1943 },
   { id: 2, name: 'Alan Rickman', birthYear: 1946 },
   { id: 3, name: 'Serhii Plokhiy', birthYear: 1957 },
 ];
 const bookService: IBookService = {
   getBookById: id => books.find(book => book.id === id),
-  getAutorById: id => authors.find(author => author.id === id),
+  getAuthorById: id => authors.find(author => author.id === id),
   getAllBooks() {
     return books;
   },

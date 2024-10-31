8d170a1b49d1e8d5c8a029aeb5b77f9cb4474c61
Nataly Dugasova <dugasova@gmail.com>
2024-10-31 14:48:14 +0200

    Added new feature

--- src/homework-12.1/index.ts
+++ src/homework-12.1/index.ts
@@ -14,133 +14,131 @@ type GridFilterSetValues<T> = {
   values: T[];
 };
 
-interface Movie {
+interface IMovie {
   title: string;
   releaseYear: number;
   rating: number;
   awards: string[];
 }
 
-interface Category {
+interface ICategory {
   name: string;
-  movies: Movie[];
+  movies: IMovie[];
 }
 
-interface MovieFilter {
+interface IMovieFilter {
   titleFilter?: GridFilterValue<string>;
   releaseYearFilter?: GridFilterValue<number>;
   ratingFilter?: GridFilterValue<number>;
   awardsFilter?: GridFilterSetValues<string>;
 }
 
-interface CategoryFilter {
-  nameFilter?: GridFilterValue<string>;
-  moviesFilter?: MovieFilter;
+interface ICategoryFilter {
+  nameFilter: GridFilterSetValues<string>;
 }
 
-abstract class BaseList<T, F> {
-  protected items: T[];
-  protected currentFilter: F;
+class MovieList {
+  private movies: IMovie[];
+  private filters: IMovieFilter;
 
-  constructor(items: T[]) {
-    this.items = items;
-    this.currentFilter = {} as F;
+  public constructor(movies: IMovie[]) {
+    this.movies = movies;
+    this.filters = {
+      titleFilter: { type: GridFilterTypeEnum.ExactMatch, filter: '' },
+      releaseYearFilter: { type: GridFilterTypeEnum.Range, filter: 0, filterTo: 0 },
+      ratingFilter: { type: GridFilterTypeEnum.Range, filter: 0, filterTo: 0 },
+      awardsFilter: { values: [] },
+    };
   }
 
-  abstract applySearchValue(query: string): T[];
-  abstract applyFiltersValue(filter: F): T[];
-}
-
-class MovieList extends BaseList<Movie, MovieFilter> {
-  applySearchValue(title: string) {
-    return this.items.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+  applySearchValue(title: string): void {
+    this.filters.titleFilter = {
+      type: GridFilterTypeEnum.ExactMatch,
+      filter: title,
+    };
   }
 
-  applyFiltersValue(filter: MovieFilter) {
-    let filteredMovies = this.items;
+  applyFiltersValue(
+    filter: keyof IMovieFilter,
+    value: GridFilterSetValues<string> | GridFilterValue<number> | GridFilterValue<string>
+  ): void {
+    switch (filter) {
+      case 'titleFilter':
+        this.filters[filter] = value as GridFilterValue<string>;
+        break;
 
-    if (filter.titleFilter) {
-      const { filter: titleFilter } = filter.titleFilter;
+      case 'releaseYearFilter':
+      case 'ratingFilter':
+        this.filters[filter] = value as GridFilterValue<number>;
+        break;
+
+      case 'awardsFilter':
+        this.filters[filter] = value as GridFilterSetValues<string>;
+    }
+  }
+
+  public getMovies(): IMovie[] {
+    let filteredMovies = this.movies;
+
+    let titleFilter = this.filters.titleFilter?.filter as string;
+    if (titleFilter !== '') {
       filteredMovies = filteredMovies.filter(movie => movie.title.toLowerCase().includes(titleFilter.toLowerCase()));
     }
 
-    if (filter.releaseYearFilter) {
-      const { filter: yearFilter, filterTo } = filter.releaseYearFilter;
-      filteredMovies = filteredMovies.filter(movie =>
-        filterTo ? movie.releaseYear >= yearFilter && movie.releaseYear <= filterTo : movie.releaseYear === yearFilter
-      );
+    let fromFilter = this.filters.releaseYearFilter?.filter as number;
+    let toFilter = this.filters.releaseYearFilter?.filterTo as number;
+    if (toFilter >= fromFilter && fromFilter + toFilter > 0) {
+      filteredMovies = filteredMovies.filter(movie => movie.releaseYear >= fromFilter && movie.releaseYear <= toFilter);
     }
 
-    if (filter.ratingFilter) {
-      const { filter: ratingFilter, filterTo } = filter.ratingFilter;
-      filteredMovies = filteredMovies.filter(movie =>
-        filterTo ? movie.rating >= ratingFilter && movie.rating <= filterTo : movie.rating === ratingFilter
-      );
+    fromFilter = this.filters.ratingFilter?.filter as number;
+    toFilter = this.filters.ratingFilter?.filterTo as number;
+    if (toFilter >= fromFilter && fromFilter + toFilter > 0) {
+      filteredMovies = filteredMovies.filter(movie => movie.rating >= fromFilter && movie.rating <= toFilter);
     }
 
-    if (filter.awardsFilter) {
-      const { values } = filter.awardsFilter;
-      filteredMovies = filteredMovies.filter(movie => values.some(award => movie.awards.includes(award)));
-    }
+    let awards = this.filters.awardsFilter?.values;
+    filteredMovies = filteredMovies.filter(
+      movie => this.filters.awardsFilter?.values.some(i => movie.awards.includes(i))
+    );
 
     return filteredMovies;
   }
 }
 
-class CategoryList extends BaseList<Category, CategoryFilter> {
-  applySearchValue(name: string) {
-    return this.items.filter(category => category.name.toLowerCase().includes(name.toLowerCase()));
+class CategoryList {
+  private categories: ICategory[];
+  private filter: ICategoryFilter;
+
+  public constructor(categories: ICategory[]) {
+    this.categories = categories;
+    this.filter = { nameFilter: { values: [] } };
   }
 
-  applyFiltersValue(filter: CategoryFilter) {
-    let filteredCategories = this.items;
+  applySearchValue(names: string[]): void {
+    this.filter = { nameFilter: { values: names } };
+  }
 
-    if (filter.nameFilter) {
-      const { filter: nameFilter } = filter.nameFilter;
-      filteredCategories = filteredCategories.filter(category =>
-        category.name.toLowerCase().includes(nameFilter.toLowerCase())
-      );
-    }
-
-    if (filter.moviesFilter) {
-      const movieFilter = filter.moviesFilter;
-      filteredCategories = filteredCategories.filter(category =>
-        category.movies.some(movie => {
-          const movieList = new MovieList(category.movies);
-          return movieList.applyFiltersValue(movieFilter).length > 0;
-        })
-      );
-    }
-
-    return filteredCategories;
+  public getCategories(): ICategory[] {
+    return this.categories.filter(category => this.filter.nameFilter.values.includes(category.name));
   }
 }
 
-const movies: Movie[] = [
+const movies: IMovie[] = [
   { title: 'Top Gun', releaseYear: 2022, rating: 8.2, awards: ['Oscar', 'BAFTA'] },
   { title: 'The Wolf of Wall Street', releaseYear: 2013, rating: 8.8, awards: ['Oscar'] },
   { title: 'Oppenheimer', releaseYear: 2023, rating: 8.6, awards: ['Oscar'] },
 ];
 
-const categories: Category[] = [{ name: 'Sci-Fi', movies: movies }];
-
+const categories: ICategory[] = [{ name: 'Sci-Fi', movies: movies }];
 const movieList = new MovieList(movies);
 const categoryList = new CategoryList(categories);
+movieList.applySearchValue('Oppenheimer');
+movieList.applyFiltersValue('titleFilter', { type: GridFilterTypeEnum.ExactMatch, filter: 'The Wolf of Wall Street' });
+movieList.applyFiltersValue('releaseYearFilter', { type: GridFilterTypeEnum.Range, filter: 2010, filterTo: 2014 });
+movieList.applyFiltersValue('ratingFilter', { type: GridFilterTypeEnum.Range, filter: 3.6, filterTo: 8.8 });
+movieList.applyFiltersValue('awardsFilter', { values: ['Oscar'] });
 
-const searchedMovies = movieList.applySearchValue('The Wolf of Wall Street');
-
-const searchedCategories = categoryList.applySearchValue('Sci-Fi');
-
-const filteredMovies = movieList.applyFiltersValue({
-  titleFilter: { type: GridFilterTypeEnum.ExactMatch, filter: 'The Wolf of Wall Street' },
-  releaseYearFilter: { type: GridFilterTypeEnum.Range, filter: 2010, filterTo: 2014 },
-  ratingFilter: { type: GridFilterTypeEnum.ExactMatch, filter: 8.2 },
-  awardsFilter: { values: ['Oscar'] },
-});
-
-const filteredCategories = categoryList.applyFiltersValue({
-  nameFilter: { type: GridFilterTypeEnum.ExactMatch, filter: 'Sci-Fi' },
-  moviesFilter: {
-    titleFilter: { type: GridFilterTypeEnum.ExactMatch, filter: 'The Wolf of Wall Street' },
-  },
-});
+categoryList.applySearchValue(['Sci-Fi']);
+console.log(movieList.getMovies());
+console.log(categoryList.getCategories());

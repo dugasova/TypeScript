a61a0b56b3c43144330b82798673978ed4351834
Nataly Dugasova <dugasova@gmail.com>
2024-11-07 16:36:23 +0200

    Added abstract class for TodoItem, extends todoList

--- src/homework-14.1/index.ts
+++ src/homework-14.1/index.ts
@@ -1,159 +1,166 @@
-enum ItemStatusEnum {
-  New,
-  InProgress,
-  Done,
-}
-
+type Uuid = string;
 interface ITodoItem {
-  id: string;
+  id: Uuid;
   title: string;
   content: string;
   createdAt: Date;
-  editedAt?: Date;
+  editedAt?: Date | null;
   isDone: boolean;
-  status: ItemStatusEnum;
+  editItem: (payload: ITodoItemEdited) => void;
 }
 
-class TodoItem implements ITodoItem {
-  readonly id: string = Math.random().toString(36).substring(2, 9);
-  public title: string = "";
-  public content: string = "";
-  readonly createdAt = new Date();
-  public editedAt?: Date | undefined;
-  public isDone: boolean = false;
-  public status: ItemStatusEnum = ItemStatusEnum.New;
+interface ITodoItemEdited {
+  title: ITodoItem['title'];
+  content: ITodoItem['content'];
+}
 
-  constructor(title: string, content: string, status: ItemStatusEnum) {
+interface ITodoList {
+  addItem: (item: BaseTodoItem) => ITodoItem;
+  removeItem: (id: Uuid) => ITodoItem | null;
+  editItem: (id: Uuid, payload: ITodoItemEdited) => ITodoItem;
+  getItemById: (id: Uuid) => ITodoItem | null;
+  getAllItems: () => ITodoItem[];
+  numberOfTasks: number;
+  itemInProgress: number;
+  setTaskDone: (id: Uuid) => void;
+}
+
+abstract class BaseTodoItem implements ITodoItem {
+  readonly id: Uuid = Math.random().toString(36).substring(2, 9);
+  readonly createdAt = new Date();
+  public editedAt?: Date | null = null;
+  public isDone: boolean = false;
+
+  constructor(
+    public title: string,
+    public content: string
+  ) {
     if (!title.trim() || !content.trim()) {
-        throw new Error('Task cannot be empty!');
-      }
-  
-    this.status = status;
-    this.content = content;
-    this.title = title;
-    this.isDone = false;
+      throw new Error('Todo item cannot be empty');
+    }
   }
 
-  public editItem(newTitle: string, newContent: string): void {
+  public complete(): void {
+    this.isDone = true;
+  }
+
+  public abstract editItem({ title, content }: ITodoItemEdited): void;
+}
+
+class TodoItem extends BaseTodoItem {
+  public override editItem({ title, content }: ITodoItemEdited): void {
+    throw new Error('TodoItem cannot be edited');
+  }
+}
+
+class ConfirmedTodoItem extends BaseTodoItem {
+  public editItem({ title, content }: ITodoItemEdited): void {
     if (this.title.trim()) {
-      this.title = newTitle;
+      this.title = title;
     }
 
     if (this.content.trim()) {
-      this.content = newContent;
+      this.content = content;
     }
 
     this.editedAt = new Date();
   }
+}
 
-  public changeStatus(): void {
-    this.status = ItemStatusEnum.Done;
+class TodoList implements ITodoList {
+  protected items: ITodoItem[] = [];
+
+  public addItem(item: BaseTodoItem): ITodoItem {
+    this.items.push(item);
+    return { ...item } as BaseTodoItem;
   }
-}
 
-class ConfirmeedTodoItem extends TodoItem {
-    private isConfirmed: boolean = false;
-
-    constructor(content: string, title: string) {
-        super(title, content, ItemStatusEnum.New);
+  public getItemById(id: Uuid): ITodoItem {
+    const item = this.items[this.findByIndexItem(id)];
+    if (!item) {
+      throw new Error('Task not found');
     }
-
-    public confirm(): void {
-        this.isConfirmed = true;
-    }
-
-    public editItem(newTitle: string, newContent: string): void {
-        if (!this.isConfirmed)
-            throw new Error('Task is not confirmed!');
-        
-        super.editItem(newTitle, newContent);
-    }
-}
-
-class TodoList {
-  items: { [id: string]: TodoItem } = {};
-
-  public addNewItem(item: TodoItem): void {
-    this.items[item.id] = item;
+    return item;
   }
 
   public get numberOfTasks(): number {
-    return Object.keys(this.items).length;
+    return this.items.length;
   }
 
   public get itemInProgress(): number {
-    return Object.entries(this.items).filter(([, item]) => item.isDone === false).length;
+    return this.items.filter(item => !item.isDone).length;
   }
 
-  public getItem(id: string): TodoItem | undefined {
-    return this.items[id];
-  }
-
-  public editTask(id: string, newTitle: string, newContent: string): void {
-    this.items[id].editItem(newTitle, newContent);
-  }
-
-  public setTaskDone(id: string): void { 
-    this.items[id].isDone = true;
-  }
-
-  public removeItem(id: string): void {
-    delete this.items[id];
-  }
-
-  public get allItems(): TodoItem[] {
-    return Object.values(this.items);
-  }  
-}
-
-enum sortByEnum {
-    date,
-    status
-}
-  
-class SortedTodoList extends TodoList {
-  protected sortBy: sortByEnum;
-
-  constructor(sortBy: sortByEnum) {
-    super();
-    this.sortBy = sortBy;
-  }
-
-  public get allItems(): TodoItem[] {
-    switch( this.sortBy ) {
-      case sortByEnum.date:
-        return super.allItems.sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
-      case sortByEnum.status:
-        return super.allItems.sort((a, b) => b.status - a.status);
+  public editItem(id: Uuid, payload: ITodoItemEdited): ITodoItem {
+    const itemIndex = this.findByIndexItem(id);
+    if (itemIndex < 0) {
+      throw new Error(`${id} is not found`);
     }
+
+    const item = this.items[itemIndex];
+    item.editItem(payload);
+
+    return { ...item };
+  }
+
+  public setTaskDone(id: Uuid): void {
+    const itemIndex = this.findByIndexItem(id);
+    const item = this.items[itemIndex];
+    item.isDone;
+  }
+
+  public removeItem(id: Uuid): ITodoItem {
+    const itemIndex = this.findByIndexItem(id);
+    return this.items.splice(itemIndex, 1)[0];
+  }
+
+  public getAllItems(): ITodoItem[] {
+    return this.items;
+  }
+
+  private findByIndexItem(id: Uuid): number {
+    const itemIndex = this.items.findIndex(item => item.id === id);
+    if (itemIndex < 0) {
+      throw new Error(`${id} is not found`);
+    }
+
+    return itemIndex;
+  }
+}
+
+class SortedTodoList extends TodoList {
+  public sortByStatus(): ITodoItem[] {
+    let result = this.items.sort((a, b) => Number(b.isDone) - Number(a.isDone));
+    return result;
+  }
+  public sortByDateCreated(): ITodoItem[] {
+    return this.getAllItems().sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime());
   }
 }
 
 class SearchableTodoList extends TodoList {
-    public searchByTitle(title: string): TodoItem[] {
-        return Object.values(this.items).filter((item) => item.title.includes(title));
-    }
-
-    public searchByContent(content: string): TodoItem[] {
-        return Object.values(this.items).filter((item) => item.content.includes(content));
-    }
+  public searchByTitle(title: string): ITodoItem[] {
+    return this.getAllItems().filter(item => item.title.includes(title));
   }
-  
-const todoList = new SortedTodoList(sortByEnum.status);
 
-const task1 = new TodoItem('Task 1', 'Task 1 content', ItemStatusEnum.New);
-todoList.addNewItem(task1);
+  public searchByContent(content: string): ITodoItem[] {
+    return this.getAllItems().filter(item => item.content.includes(content));
+  }
+}
 
-const task2 = new TodoItem('Task 2', 'Task 2 content', ItemStatusEnum.InProgress);
-todoList.addNewItem(task2);
-todoList.setTaskDone(task2.id);
+const todoList = new SearchableTodoList();
 
-const task3 = new ConfirmeedTodoItem('Task 3', 'Task 3 content');
-todoList.addNewItem(task3);
-task3.confirm();
-todoList.editTask(task3.id, 'Task 3 edited', 'Task 3 content edited');
+const task1 = new TodoItem('Task 1', 'Task 1 content');
+todoList.addItem(task1);
 
+const task2 = new TodoItem('Task 2', 'Task 2 content');
+task2.complete();
+todoList.addItem(task2);
+
+const task3 = new ConfirmedTodoItem('Task 3', 'Task 3 content');
+todoList.addItem(task3);
+const editedItem = todoList.editItem(task3.id, { title: 'Task 3 edited', content: 'Task 3 content edited' });
+
+const allItems = todoList.searchByContent('Task 3 content edited');
+console.log(allItems);
 console.log(todoList.numberOfTasks);
-console.log(todoList.itemInProgress);
-console.log(todoList.allItems);
-
